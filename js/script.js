d1efebf28baa4e4e0df98f0c040543acd5e0b68c
Colin McKean <ezikill@gmail.com>
2016-09-30 17:21:21 +0100

    add css and js functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+updateOutput();
 $(".toggleButton").hover(function(){
     $(this).addClass("highlightedButton");
 }, function(){
@@ -7,6 +8,10 @@ $(".toggleButton").hover(function(){
 $(".toggleButton").click(function(){
     $(this).toggleClass("active");
     $(this).removeClass("highlightedButton");
+    var panelID =$(this).attr("id") + "Panel";
+    $("#"+panelID).toggleClass("hidden");
+    var numberOfActivePanels = 4 - $(".hidden").length;
+    $(".panel").width(($(window).width() / numberOfActivePanels) -20)
 })
 
 $(".panel").height($(window).height() - $("#header").height() -20);
@@ -14,4 +19,9 @@ $(".panel").width(($(window).width()/2) -10);
 
 $("textarea").on('change keyup paste', function(){
     $("iframe").contents().find("html").html($("#htmlPanel").val());
+    updateOutput();
 });
+
+function updateOutput(){
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>"+$("#cssPanel").val() + "</style></head></html><body>"+$("#htmlPanel").val()+"</body></html>");
+}
